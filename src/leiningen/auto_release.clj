34b50b294345533c4a3f574e6bdcbdb9b8be4699
Andrew Mcveigh <me@andrewmcveigh.com>
2014-10-26 21:01:23 +0100

    Declare update release notes fn

--- src/leiningen/auto_release.clj
+++ src/leiningen/auto_release.clj
@@ -1,5 +1,6 @@
 (ns leiningen.auto-release
   (:require
+   [clojure.java.io :as io]
    [leiningen.core.eval :as eval]))
 
 (defn checkout [{:keys [root]} branch]
@@ -13,3 +14,12 @@
 (defn merge [{:keys [root]} branch]
   (binding [eval/*dir* root]
     (eval/sh "git" "merge" branch "--no-edit")))
+
+(defn update-release-notes [{:keys [root version]}]
+  
+  )
+
+;; (let [file (io/file "." "ReleaseNotes.md")
+;;       text (if (.exists file) (slurp file) "")]
+;;   text
+;;   )
