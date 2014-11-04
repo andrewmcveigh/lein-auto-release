b96583a585bb50a33b6d3fb653f5c77ecfe2701a
Andrew Mcveigh <me@andrewmcveigh.com>
2014-11-04 21:27:48 +0100

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.andrewmcveigh/lein-auto-release "0.1.6"
+(defproject com.andrewmcveigh/lein-auto-release "0.1.7-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
